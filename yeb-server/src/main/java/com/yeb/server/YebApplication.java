37445b89ee7599d8e4a69e1c772f710bde69471c
lsz <lsz@123.cn>
2021-09-09 13:41:58 +0800

    four commit

--- yeb-server/src/main/java/com/yeb/server/YebApplication.java
+++ yeb-server/src/main/java/com/yeb/server/YebApplication.java
@@ -1,6 +1,7 @@
 package com.yeb.server;
 
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -9,6 +10,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  *启动类
  */
 @SpringBootApplication
+@MapperScan("com.yeb.server.mapper")
 public class YebApplication {
     public static void main(String[] args) {
 
