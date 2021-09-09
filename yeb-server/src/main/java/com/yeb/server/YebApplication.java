dfef7592cae27f98508f3987a8307fcda8b7c889
lsz <lsz@123.cn>
2021-09-09 13:38:29 +0800

    third commit

---
+++ yeb-server/src/main/java/com/yeb/server/YebApplication.java
@@ -0,0 +1,17 @@
+package com.yeb.server;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+/**
+ *启动类
+ */
+@SpringBootApplication
+public class YebApplication {
+    public static void main(String[] args) {
+
+        SpringApplication.run(YebApplication.class,args);
+    }
+}
